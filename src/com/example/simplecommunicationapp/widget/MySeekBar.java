b7224bc68e758d6433f07153d6d86ef08b023063
Fun <fun_wudy@163.com>
2016-08-16 22:16:19 +0800

    what a disgusting night 8.16

--- src/com/example/simplecommunicationapp/widget/MySeekBar.java
+++ src/com/example/simplecommunicationapp/widget/MySeekBar.java
@@ -2,11 +2,12 @@ package com.example.simplecommunicationapp.widget;
 
 import com.example.simplecommunicationapp.R;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -18,7 +19,7 @@ public class MySeekBar extends LinearLayout {
 	private TextView leftText;
 	private TextView rightText;
 	private TextView curText;
-	
+
 	private TextMoveLayout mTextMoveLayout;
 
 	double moveStep;
@@ -32,15 +33,15 @@ public class MySeekBar extends LinearLayout {
 		LayoutInflater.from(context).inflate(R.layout.my_seek_bar, this);
 		seekBar = (SeekBar) findViewById(R.id.seek_bar);
 		leftText = (TextView) findViewById(R.id.left_text);
+		leftText.setText("0");
 		rightText = (TextView) findViewById(R.id.right_text);
-		curText = new TextView(context);
-		mTextMoveLayout = (TextMoveLayout) findViewById(R.id.text_move_layout);
-		ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(200, 50);
-		mTextMoveLayout.addView(curText, layoutParams);
-		curText.setText("hehe");
-		curText.layout(0, 20, 200, 80);
+		rightText.setText(String.valueOf(seekBar.getMax()));
+		curText = (TextView) findViewById(R.id.cur_text);
+		curText.setText(String.valueOf(seekBar.getProgress()));
+		curText.layout(0, 0, 30, 30);
 
 		seekBar.setOnSeekBarChangeListener(onSeekBarChangeListener);
+
 	}
 
 	private OnSeekBarChangeListener onSeekBarChangeListener = new OnSeekBarChangeListener() {
@@ -53,22 +54,15 @@ public class MySeekBar extends LinearLayout {
 
 		@Override
 		public void onStartTrackingTouch(SeekBar seekBar) {
-			// TODO Auto-generated method stub
-
+			int spaceWidth = getWidth() - curText.getWidth();
+			moveStep = (double) spaceWidth / seekBar.getMax();
 		}
 
 		@Override
 		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-			// TODO Auto-generated method stub
-			int layoutWidth = getWidth();
-			moveStep = (double) layoutWidth / seekBar.getMax();
-			rightText.setText(String.valueOf(layoutWidth));
-
 			int offset = (int) (progress * moveStep);
-			leftText.setText(String.valueOf(offset));
 			curText.setText(String.valueOf(progress));
 			curText.layout(offset, curText.getTop(), curText.getWidth() + offset, curText.getBottom());
-			//curText.layout(getLeft() + offset, getTop(), getRight() + offset, getBottom());
 		}
 	};
 
